538ae84e7a0e3d01ad02bb82e62a65ce353d079a
Graham <iampapagray@gmail.com>
2019-04-28 13:41:04 +0000

    Completed initial page of UI

---
+++ lib/data.dart
@@ -0,0 +1,8 @@
+var images = [
+  "assets/image_01.png",
+  "assets/image_02.png",
+  "assets/image_03.png",
+];
+
+var title = ["Hemes Armchair", "Sofa Armchair", " Wooden Armchair"];
+var price= ["126", "148", "179"];
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:furniture_ui/Custom_Icons.dart';
+import 'package:furniture_ui/data.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,105 +9,244 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Furniture UI',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  int _currentIndex = 0;
+
+  Widget _buildGradientContainer(double width, double height){
+    return Align(
+      alignment: Alignment.topRight,
+      child: Container(
+        width: width * 0.8,
+        height: height / 2,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color(0xFFfbfcfd), Color(0xFFf2f3f8)],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            stops: [0.5, 1.0]
+          )
+        ),
+      ),
+    );
+  }
+
+  Widget _buildAppBar(){
+    return Positioned(
+      top: 40.0,
+      left: 20.0,
+      right: 20.0,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          IconButton(
+            icon: Icon(CustomIcons.menu, color: Colors.black),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(CustomIcons.search, color: Colors.black),
+            onPressed: () {},
+          ),
+        ],
+      )
+    );
+  }
+
+  Widget _buildTitle(double height){
+    return Positioned(
+      top: height * 0.2,
+      left: 30.0,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text("Wooden Armchair", 
+            style: TextStyle(fontSize: 28.0, fontFamily: "Montserrat-Bold")),
+
+          Text("Lorem Ipsum",
+            style: TextStyle(fontSize: 16.0, fontFamily: "Montserrat-Medium" )),
+
+        ],
+      )
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      backgroundColor: Color(0xFFf2f3f8),
+      body: LayoutBuilder(
+        builder: (context, constraint){
+          var width = constraint.maxWidth;
+          var height = constraint.maxHeight;
+
+          return Stack(
+            fit: StackFit.expand,
+            children: <Widget>[
+              _buildGradientContainer(width, height),
+              _buildAppBar(),
+              _buildTitle(height),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: SizedBox(
+                  height: height * 0.6,
+                  child: ListView.builder(
+                    itemCount: images.length,
+                    scrollDirection: Axis.horizontal,
+                    physics: BouncingScrollPhysics(),
+                    itemBuilder: (context, index){
+                      return Padding(
+                        padding: EdgeInsets.only(left: 35.0, bottom: 60.0),
+                        child: SizedBox(
+                          width: 200.0,
+                          child: Stack(
+                            fit: StackFit.expand,
+                            children: <Widget>[
+                              Padding(
+                                padding: EdgeInsets.only(top: 45.0),
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    color: (index % 2 == 0)
+                                        ? Colors.white
+                                        : Color(0xFF2a2d3f),
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color: Colors.black12,
+                                        offset: Offset(0.0, 10.0),
+                                        blurRadius: 10.0
+                                      )
+                                    ],
+                                    borderRadius: BorderRadius.circular(12.0),
+                                  ),
+                                )
+                              ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: <Widget>[
+                                  Image.asset(
+                                    images[index],
+                                    width: 172.5,
+                                    height: 199.0,
+                                  ),
+                                  SizedBox(
+                                    height: 12.0,
+                                  )
+                                  ,
+                                  Padding(
+                                    padding: EdgeInsets.all(12.0),
+                                    child: Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: <Widget>[
+                                        Text(title[index], 
+                                            style: TextStyle(
+                                              fontSize: 12.0,
+                                              fontFamily: "Montserrat-Bold",
+                                              color: (index % 2 == 0)
+                                                  ? Color(0xFF2a2d3f)
+                                                  : Colors.white,
+                                            )),
+                                        SizedBox(
+                                          height: 8.0
+                                        ),
+                                        Text("NEW SELL",
+                                            style: TextStyle(
+                                              fontSize: 12.0,
+                                              fontFamily: "Montserrat-Medium",
+                                              color: (index % 2 == 0)
+                                                  ? Color(0xFF2a2d3f)
+                                                  : Colors.white,
+                                            )),
+                                        SizedBox(
+                                          height: 40.0
+                                        ),
+                                        Text("\$ " + price[index],
+                                            style: TextStyle(
+                                              fontSize: 30.0,
+                                              fontFamily: "Montserrat-Bold",
+                                              color: (index % 2 == 0)
+                                                  ? Color(0xFF2a2d3f)
+                                                  : Colors.white,
+                                            )),
+                                        
+                                      ],
+                                    ),
+                                  )
+                                ],
+                              )
+                            ],
+                          )
+                        ),
+                      );
+                    }
+                  ),
+                )
+              )
+            ],
+          );
+        },
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      bottomNavigationBar: BottomNavigationBar(
+        // selectedItemColor: Colors.black,
+        onTap: (index){
+          setState((){
+            _currentIndex = index;
+          });
+        },
+        currentIndex: _currentIndex,
+        items : [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.panorama_horizontal),
+            title: Container(
+              height: 0.0,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.bookmark_border),
+            title: Container(
+              height: 0.0,
             ),
-          ],
+          ),
+        ]
+      ),
+      floatingActionButton: Container(
+        width: 65.0,
+        height: 65.0,
+        decoration: BoxDecoration(
+          color: Color(0xFFfa7b58),
+          shape: BoxShape.circle,
+          boxShadow: [
+            BoxShadow(
+              color: Color(0xFFf78a6c).withOpacity(0.6),
+              offset: Offset(0.0,10.0),
+              blurRadius: 10.0,
+            ),
+          ]
+        ),
+        child: RawMaterialButton(
+          shape: CircleBorder(),
+          child: Icon(
+            Icons.add,
+            size: 35.0,
+            color: Colors.white,
+          ),
+          onPressed: () {},
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
